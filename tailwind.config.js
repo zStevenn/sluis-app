1d7b2be631a245800583e11de5323f8ca939f16e
Steven <steventje678@hotmail.com>
2023-04-20 23:10:13 +0200

    Adds primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#11470D",
+				secondary: "#430D47",
+			},
+			fontFamily: {
+				outfit: ["Outfit", "sans-serif"],
+			},
+		},
+		plugins: [],
 	},
-	plugins: [],
 }
