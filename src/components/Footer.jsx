ed0027e0088ad7d825d09c3d490139112162b64b
Steven <steventje678@hotmail.com>
2023-04-21 22:54:06 +0200

    Adds responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { MdKeyboardDoubleArrowRight } from "react-icons/md"
 import { Link } from "react-router-dom"
+import Container from "./Container"
 
 function FooterTitle({ children, className = "" }) {
 	return <h3 className={`text-xl text-neutral-100 ${className}`}>{children}</h3>
@@ -26,46 +27,48 @@ function FooterLink({ children, to, className = "" }) {
 
 export default function Footer() {
 	return (
-		<footer className="bg-primary px-8 py-8 mt-8 flex flex-col gap-8">
-			{/* Adress details */}
-			<div>
-				<FooterTitle className="mb-2">Adresgegevens</FooterTitle>
-				<FooterText>De Sluis 25, 3972 VC</FooterText>
-				<FooterText>Driebergen-Rijsenburg</FooterText>
-				<FooterText className="pb-3">Utrecht, Nederland</FooterText>
-				<FooterText>(0343) - 531 646</FooterText>
-				<FooterText className="underline">
-					<a
-						alt="Mail naar info@cafetariadesluis.nl"
-						title="Mail naar info@cafetariadesluis.nl"
-						href="mailto:info@cafetariadesluis.nl"
-					>
-						info@cafetariadesluis.nl
-					</a>
-				</FooterText>
-			</div>
-			{/* Openingtimes */}
-			<div>
-				<FooterTitle className="mb-2">Openingstijden</FooterTitle>
-				<FooterText>Maandag: 12:00 - 21:00</FooterText>
-				<FooterText>Dinsdag: 12:00 - 21:00</FooterText>
-				<FooterText>Woensdag: 12:00 - 21:00</FooterText>
-				<FooterText>Donderdag: 12:00 - 21:00</FooterText>
-				<FooterText>Vrijdag: 12:00 - 21:00</FooterText>
-				<FooterText>Zaterdag: 12:00 - 21:00</FooterText>
-				<FooterText>Zondag: 16:00 - 21:00</FooterText>
-			</div>
-			{/* Links */}
-			<div>
-				<FooterTitle className="mb-2">Links</FooterTitle>
-				<FooterLink to="/bestellen">Bestellen</FooterLink>
-				<FooterLink to="/menu">Menu</FooterLink>
-				<FooterLink to="/contact">Contact</FooterLink>
-				<FooterLink to="/nieuws">Nieuws</FooterLink>
-				<FooterLink to="/faq">FAQ</FooterLink>
-				<FooterLink to="/privacybeleid">Privacybeleid</FooterLink>
-				<FooterLink to="/sitemap">Sitemap</FooterLink>
-			</div>
+		<footer className="bg-primary px-8 py-8 mt-8">
+			<Container className="flex flex-col md:flex-row md:justify-between gap-8">
+				{/* Adress details */}
+				<div>
+					<FooterTitle className="mb-2">Adresgegevens</FooterTitle>
+					<FooterText>De Sluis 25, 3972 VC</FooterText>
+					<FooterText>Driebergen-Rijsenburg</FooterText>
+					<FooterText className="pb-3">Utrecht, Nederland</FooterText>
+					<FooterText>(0343) - 531 646</FooterText>
+					<FooterText className="underline">
+						<a
+							alt="Mail naar info@cafetariadesluis.nl"
+							title="Mail naar info@cafetariadesluis.nl"
+							href="mailto:info@cafetariadesluis.nl"
+						>
+							info@cafetariadesluis.nl
+						</a>
+					</FooterText>
+				</div>
+				{/* Openingtimes */}
+				<div>
+					<FooterTitle className="mb-2">Openingstijden</FooterTitle>
+					<FooterText>Maandag: 12:00 - 21:00</FooterText>
+					<FooterText>Dinsdag: 12:00 - 21:00</FooterText>
+					<FooterText>Woensdag: 12:00 - 21:00</FooterText>
+					<FooterText>Donderdag: 12:00 - 21:00</FooterText>
+					<FooterText>Vrijdag: 12:00 - 21:00</FooterText>
+					<FooterText>Zaterdag: 12:00 - 21:00</FooterText>
+					<FooterText>Zondag: 16:00 - 21:00</FooterText>
+				</div>
+				{/* Links */}
+				<div>
+					<FooterTitle className="mb-2">Links</FooterTitle>
+					<FooterLink to="/bestellen">Bestellen</FooterLink>
+					<FooterLink to="/menu">Menu</FooterLink>
+					<FooterLink to="/contact">Contact</FooterLink>
+					<FooterLink to="/nieuws">Nieuws</FooterLink>
+					<FooterLink to="/faq">FAQ</FooterLink>
+					<FooterLink to="/privacybeleid">Privacybeleid</FooterLink>
+					<FooterLink to="/sitemap">Sitemap</FooterLink>
+				</div>
+			</Container>
 		</footer>
 	)
 }
