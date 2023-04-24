eea1252722618777248246a94ac206b40b72324b
Steven <steventje678@hotmail.com>
2023-04-24 21:25:17 +0200

    Creates Internal and external CTA

--- src/components/Card/CardCTA.jsx
+++ src/components/Card/CardCTA.jsx
@@ -1,6 +1,20 @@
 import { MdKeyboardDoubleArrowRight } from "react-icons/md"
+import { Link } from "react-router-dom"
 
-export default function CardCTA({ to, product }) {
+export function ICardCTA({ to, title }) {
+	return (
+		<Link
+			to={to}
+			alt={title}
+			title={title}
+			className="cursor-pointer hover:scale-110 transition-all duration-150 ease-in hover:text-primary"
+		>
+			<MdKeyboardDoubleArrowRight className="text-3xl" />
+		</Link>
+	)
+}
+
+export function ECardCTA({ to, product }) {
 	return (
 		<a
 			href={to}
