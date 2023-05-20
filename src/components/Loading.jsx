5c660c8cab05c54219c03e74a457d3a76de46433
Steven <steventje678@hotmail.com>
2023-05-20 14:08:54 +0200

    Adds container to loading skeleton

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,28 +1,29 @@
+import Container from "./Container"
+
 export default function Loading() {
 	return (
-		<div className="h-[85vh] flex flex-col mx-8 pt-8 gap-4 animate-pulse">
+		<Container className="h-[85vh] flex flex-col mx-8 pt-8 gap-4 animate-pulse">
 			<div className="h-9 w-1/2 rounded bg-neutral-300/90"></div>
 			<div className="grid gap-2">
-
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
-			<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
+				<div className="h-4 w-full rounded bg-neutral-300/90"></div>
 			</div>
-		</div>
+		</Container>
 	)
 }
